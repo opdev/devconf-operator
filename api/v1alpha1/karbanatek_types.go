ab0838eefab8999db8f3afd0dae197dc0aec1147
Yuri Sa <yurimsa@gmail.com>
2024-06-05 11:35:07 +0200

    First Commit
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>

---
+++ api/v1alpha1/karbanatek_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// KarbanatekSpec defines the desired state of Karbanatek
+type KarbanatekSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Karbanatek. Edit karbanatek_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// KarbanatekStatus defines the observed state of Karbanatek
+type KarbanatekStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Karbanatek is the Schema for the karbanateks API
+type Karbanatek struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KarbanatekSpec   `json:"spec,omitempty"`
+	Status KarbanatekStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// KarbanatekList contains a list of Karbanatek
+type KarbanatekList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Karbanatek `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Karbanatek{}, &KarbanatekList{})
+}
