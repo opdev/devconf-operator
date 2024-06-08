3c0dd91298a08e4c90110f6eee10b38bc63d5ffa
Matthias Goerens <mgoerens@redhat.com>
2024-06-09 00:52:57 +0200

    Scaffold Recipe API and controller
    operator-sdk create api --group devconfcz --version v1alpha1 --kind Recipe --plugins="deploy-image/v1-alpha" --image=quay.io/rocrisp/recipe:v5 --image-container-port 5000

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	devconfczv1alpha1 "github.com/opdev/devconf-operator/api/v1alpha1"
+	"github.com/opdev/devconf-operator/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -44,6 +47,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(devconfczv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -118,6 +122,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.RecipeReconciler{
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("recipe-controller"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Recipe")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
